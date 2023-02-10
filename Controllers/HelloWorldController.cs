a1e5ce3f6c27c13d0518d220144a8a41a71524d4
take0708 <pk_er_stbo@yahoo.co.jp>
2023-02-10 21:52:06 +0900

    MVC Tutorial Part3

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -7,14 +7,16 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
-    {
-        return "This is my default action...";
-    }
+public IActionResult Index()
+{
+    return View();
+}
     // 
     // GET: /HelloWorld/Welcome/ 
-public string Welcome(string name, int ID = 1)
-{
-    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-}
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
 }
